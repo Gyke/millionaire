532ca7dead82b9883b67099ed28c3b67c1dc151d
JohnnieV <johnnie.v1986@gmail.com>
2023-02-06 17:16:23 +0400

    create rules view

--- millionaire/Controllers/ViewController.swift
+++ millionaire/Controllers/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rulesButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+    }
 }
 
