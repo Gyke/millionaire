f907398a44e8a2fd7e608f7c6e26dd8e0abbde53
Zvontsov Sergey <zvoncov@gmail.com>
2023-02-08 21:11:09 +0300

    Подсановка вопросов из базы Выбор ответа Переход на экран со статистикой

--- millionaire/Controllers/GameViewController.swift
+++ millionaire/Controllers/GameViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+
 class GameViewController: UIViewController {
 
     @IBOutlet weak var questionLabel: UILabel!
@@ -15,20 +16,37 @@ class GameViewController: UIViewController {
     @IBOutlet weak var answerThree: UILabel!
     @IBOutlet weak var answerFour: UILabel!
     
+    @IBOutlet weak var answerOneButton: UIButton!
+    @IBOutlet weak var answerTwoButton: UIButton!
+    @IBOutlet weak var answerThreeButton: UIButton!
+    @IBOutlet weak var answerFourButton: UIButton!
+    
+    @IBOutlet weak var questionNumberLabel: UILabel!
+    @IBOutlet weak var questionMoneyLabel: UILabel!
+    
     var millionaire: MillionaireProtocol!
+    
+    let money: [Int] = [100, 200, 300, 400, 500, 1000, 2000, 4000, 8000, 16000, 32000, 64000, 125000, 250000, 500000, 1000000]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        millionaire.setQuestion()
         
+        millionaire.setQuestion()
         
     }
     
     @IBAction func answerTapped(_ sender: UIButton) {
         for tag in 1...4 {
             if sender.tag == tag {
-                millionaire.answerTapped(answer: millionaire.question.answerOptions[tag - 1])
+                sender.setBackgroundImage( UIImage(named: "Rectangle purple") , for: .normal)
+        
+                answerOneButton.isUserInteractionEnabled = false
+                answerTwoButton.isUserInteractionEnabled = false
+                answerThreeButton.isUserInteractionEnabled = false
+                answerFourButton.isUserInteractionEnabled = false
+                
+                millionaire.answerTapped(answer: millionaire.question.answerOptions[tag - 1], numberOfAnswer: tag)
             }
         }
     }
@@ -36,21 +54,66 @@ class GameViewController: UIViewController {
 }
 
 extension GameViewController: MillionaireViewProtocol {
+   
     func setQuestion(question: Question) {
         questionLabel.text = question.text
         answerOne.text = question.answerOptions[0]
         answerTwo.text = question.answerOptions[1]
         answerThree.text = question.answerOptions[2]
         answerFour.text = question.answerOptions[3]
+        questionNumberLabel.text = "Question: \(millionaire.numberOfQuestion)"
+        questionMoneyLabel.text = "\(money[millionaire.numberOfQuestion - 1]) RUB"
     }
     
-    func success() {
+    func success(numberOfQuestion: Int, numberOfAnswer: Int) {
+        
+        //проигрываем музыку правильного ответа
+        
+        setButtonBackground(answerNumber: numberOfAnswer, colour: .green)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2), execute: {
+            self.performSegue(withIdentifier: "goToResult", sender: self)
+        })
         
     }
     
-    func failure() {
+    func failure(numberOfQuestion: Int, numberOfAnswer: Int) {
+        
+        //проигрываем музыку в случае неудачи
+        
+        setButtonBackground(answerNumber: numberOfAnswer, colour: .red)
+        
+        if let index = millionaire.question.answerOptions.firstIndex(where: {$0 == millionaire.question.answer}) {
+            setButtonBackground(answerNumber: index + 1, colour: .green)
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2), execute: {
+            self.performSegue(withIdentifier: "goToResult", sender: self)
+        })
         
     }
     
+    func setButtonBackground(answerNumber: Int, colour: ButtonColor) {
+        switch answerNumber {
+        case 1:
+            answerOneButton.setBackgroundImage(colour.image, for: .normal)
+        case 2:
+            answerTwoButton.setBackgroundImage(colour.image, for: .normal)
+        case 3:
+            answerThreeButton.setBackgroundImage(colour.image, for: .normal)
+        case 4:
+            answerFourButton.setBackgroundImage(colour.image, for: .normal)
+        default:
+            return
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let view = segue.destination as! ScoreTableViewController
+            view.questionNumber = millionaire.numberOfQuestion
+            view.answerResult = millionaire.answerResult
+        }
+    }
     
 }
--- millionaire/Models/Millionaire.swift
+++ millionaire/Models/Millionaire.swift
@@ -8,41 +8,63 @@
 import Foundation
 
 protocol MillionaireProtocol: AnyObject {
-    func answerTapped(answer: String)
+    func answerTapped(answer: String, numberOfAnswer: Int)
     func hintTapped()
     func setQuestion()
+    func timeIsOver()
     var question: Question {get set}
+    var answerResult: Bool {get set}
+    var numberOfQuestion: Int {get set}
     init(view: MillionaireViewProtocol, numberOfQuestion: Int)
 }
 
 protocol MillionaireViewProtocol: AnyObject {
     func setQuestion(question: Question)
-    func success()
-    func failure()
+    func success(numberOfQuestion: Int, numberOfAnswer: Int)
+    func failure(numberOfQuestion: Int, numberOfAnswer: Int)
 }
 
 class Millionaire: MillionaireProtocol {
     
     weak var view: MillionaireViewProtocol!
     var quiz = QuizMillionaire()
-    var numberOfQuestion: Int!
+    var numberOfQuestion: Int
     var question: Question
+    var answerResult: Bool
     required init(view: MillionaireViewProtocol, numberOfQuestion: Int) {
         self.view = view
         self.numberOfQuestion = numberOfQuestion
-        self.question = quiz.quiz[numberOfQuestion]
+        self.question = quiz.quiz[numberOfQuestion - 1]
+        self.answerResult = false
     }
     
-    func answerTapped(answer: String) {
-        print("\(answer)")
+    func answerTapped(answer: String, numberOfAnswer: Int) {
+        //Запускаем музыку
+        
+        //даем паузу в пять секунд
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(5), execute: {
+            if answer == self.question.answer {
+                self.answerResult = true
+                self.view.success(numberOfQuestion: self.numberOfQuestion, numberOfAnswer: numberOfAnswer)
+            } else {
+                self.view.failure(numberOfQuestion: self.numberOfQuestion, numberOfAnswer: numberOfAnswer)
+            }
+        })
+        
     }
     
     func hintTapped() {
         
     }
     
+   
+    
     public func setQuestion() {
-        self.view.setQuestion(question: quiz.quiz[numberOfQuestion])
+        self.view.setQuestion(question: quiz.quiz[numberOfQuestion - 1])
+    }
+    
+    func timeIsOver() {
+        
     }
     
 }
