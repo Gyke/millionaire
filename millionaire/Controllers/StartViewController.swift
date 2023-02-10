dbf8ab0cb81f40c161c2b1cd997b0daebd198f4d
PrMax <89717747+PrMax@users.noreply.github.com>
2023-02-10 23:25:58 +0500

    Сделал музыкальное сопровождение в игре.

--- millionaire/Controllers/StartViewController.swift
+++ millionaire/Controllers/StartViewController.swift
@@ -10,26 +10,30 @@ import UIKit
 class StartViewController: UIViewController {
     
     
-    
+    let musicStart = AudioPlayer()
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        musicStart.play(sound: "musical_intro")
         
         // Do any additional setup after loading the view.
     }
     
+    
+    
 
     @IBAction func rulesButtonPressed(_ sender: UIButton) {
         
-        
+        musicStart.stop()
         
         self.performSegue(withIdentifier: "goToRules", sender: self)
     }
     
     @IBAction func startButtonPressed(_ sender: UIButton) {
+        
+        musicStart.stop()
         
         self.performSegue(withIdentifier: "goToGame", sender: self)
     }
